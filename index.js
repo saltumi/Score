ae8a3de88c976099cd3d3d8762750e224d01588d
Mmakgwale Lebogo <MmakgwaleLebogo@capitecbank.co.za>
2024-10-22 21:30:43 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+let homeEl = document.getElementById("home-points-text");
+let guestEl = document.getElementById("guest-points-text");
+
+function addOne() {
+    homeEl.textContent = Number(homeEl.textContent) + 1;   
+}
+
+function addTwo() {
+    homeEl.textContent = Number(homeEl.textContent) + 2;
+}
+
+function addThree() {
+    homeEl.textContent = Number(homeEl.textContent) + 3;
+}
+
+
+function addGuestOne() {
+    guestEl.textContent = Number(guestEl.textContent) + 1;   
+}
+
+function addGuestTwo() {
+    guestEl.textContent = Number(guestEl.textContent) + 2;
+}
+
+function addGuestThree() {
+    guestEl.textContent = Number(guestEl.textContent) + 3;
+}
+
+
+
